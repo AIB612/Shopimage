b6bc33ce652fd161f981d884f9867155e96ca976
Mrs Branny <sherry@MrsdeMBP.fritz.box>
2026-02-10 10:28:41 +0100

    feat: add terms of service page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Privacy from "@/pages/privacy";
+import Terms from "@/pages/terms";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/privacy" component={Privacy} />
+      <Route path="/terms" component={Terms} />
       <Route component={NotFound} />
     </Switch>
   );
