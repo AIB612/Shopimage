80eccdec8fb5acfe4d5ff6e2b6fadd878ec30c13
sherryjjj <xuexuets@foxmail.com>
2026-04-13 14:04:02 +0200

    Disable runtime error overlay in production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
+    ...(!isProduction ? [runtimeErrorOverlay()] : []),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
